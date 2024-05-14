f8188f9a0ec667fa76185ac504f10df1ead55509
Dudxz7 <luizeduardo98r4@gmail.com>
2024-05-14 19:42:49 -0300

    lamp-v1

---
+++ js/lamp.js
@@ -0,0 +1,8 @@
+function acender(){
+    document.getElementById("apagada").src = "img/lamp-on.png"
+    document.body.style.backgroundColor = "white"
+}
+function apagar(){
+    document.getElementById("apagada").src = "img/lamp-off.png"
+    document.body.style.backgroundColor = "black"
+}
