1a0789d6816c55398fb71d78d3883881a59c9014
Dudxz7 <luizeduardo98r4@gmail.com>
2024-05-14 21:36:53 -0300

    lamp-v2

--- js/lamp.js
+++ js/lamp.js
@@ -1,8 +1,20 @@
-function acender(){
-    document.getElementById("apagada").src = "img/lamp-on.png"
-    document.body.style.backgroundColor = "white"
-}
-function apagar(){
-    document.getElementById("apagada").src = "img/lamp-off.png"
-    document.body.style.backgroundColor = "black"
+const lamp = document.getElementById("apagada");
+let contador = 0;
+
+function acender() {
+    contador++;
+    if (contador <= 10) {
+        if (lamp.src.endsWith("lamp-off.png")) {
+            lamp.src = "img/lamp-on.png";
+            document.getElementById("btn").innerHTML = "Apagar";
+            document.body.style.backgroundColor = "white"
+        } else {
+            lamp.src = "img/lamp-off.png";
+            document.getElementById("btn").innerHTML = "Acender";
+            document.body.style.backgroundColor = "black"
+        }
+    } else {
+        lamp.src = "img/lamp-break2.png";
+        document.getElementById("btn").innerHTML = "Quebrou";
+    }
 }
